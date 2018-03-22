3d3efe246d94e001da65b48cb01b82a489694f0e
Dharmesh Dhorajiya <im.dhorajiyadharmesh@gmail.com>
2018-03-22 17:13:08 +0530

    Add Variable

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var count = 1
+        print("counter \(count)")
+        count = count + 1
+        print("counter \(count)")
     }
 
     override func didReceiveMemoryWarning() {
